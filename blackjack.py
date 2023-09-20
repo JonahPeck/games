a964b6149a989c5257ba53aaa34d2d9bfd4cda7a
JonahPeck <PeckJonahC@gmail.com>
2023-09-20 15:54:41 -0400

    it works

--- blackjack.py
+++ blackjack.py
@@ -13,66 +13,62 @@ def card_value(card):
     else:
         return int(card[0])
 
-    random.shuffle(deck)
-    player_card = [deck.pop(), deck.pop()]
-    dealer_card = [deck.pop(), deck.pop()]
+random.shuffle(deck)
+player_card = [deck.pop(), deck.pop()]
+dealer_card = [deck.pop(), deck.pop()]
 
-    while True:
-        player_score = sum(card_value(card) for card in player_card)
-        dealer_score = sum(card_value(card) for card in dealer_card)
+while True:
+    player_score = sum(card_value(card) for card in player_card)
+    dealer_score = sum(card_value(card) for card in dealer_card)
+    print("Cards Player Has:", player_card)
+    print("Score Of The Player:", player_score)
+    print("\n")
+    choice = input('What do you want to do? ["play" to request another card, "stop" to stop]:').lower()
+    if choice == "play":
+        new_card = deck.pop()
+        player_card.append(new_card)
+    elif choice == "stop":
+        break
+    else:
+        print("Invalid choice. Please try again.")
+        continue
+    if player_score > 21:
+        print("Cards dealer has:", dealer_card)
+        print("Score of the dealer:", dealer_score)
+        print("Cards player Has:", player_card)
+        print("Score of the Player:", player_score)
+        print("Dealer wins (Player loss because player score is greater than 21)")
+        break
+    
+    while dealer_score < 17:
+        new_card = deck.pop()
+        dealer_card.append(new_card)
+        dealer_score += card_value(new_card)
+    print("Cards Dealer has:", dealer_card)
+    print("Score of the dealer:", dealer_score)
+    print("\n")
+    if dealer_score > 21:
+        print("Cards Dealer Has:", dealer_card)
+        print("Score Of The Dealer:", dealer_score)
         print("Cards Player Has:", player_card)
         print("Score Of The Player:", player_score)
-        print("\n")
-        choice = input('What do you want to do? ["play" to request another card, "stop" to stop]:').lower()
-        if choice == "play":
-            new_card = deck.pop()
-            player_card.append(new_card)
-        elif choice == "stop":
-            break
-        else:
-            print("Invalid choice. Please try again.")
-            continue
-
-        if player_score > 21:
-            print("Cards dealer has:", dealer_card)
-            print("Score of the dealer:", dealer_score)
-            print("Cards player Has:", player_card)
-            print("Score of the Player:", player_score)
-            print("Dealer wins (Player loss because player score is greater than 21)")
-            break
+        print("Player wins (Dealer Loss Because Dealer Score is exceeding 21)")
+    elif player_score > dealer_score:
+        print("Cards Dealer Has:", dealer_card)
+        print("Score Of The Dealer:", dealer_score)
+        print("Cards Player Has:", player_card)
+        print("Score Of The Player:", player_score)
+        print("Player wins (Player Has High Score than Dealer)")
+    elif dealer_score > player_score:
+        print("Cards Dealer Has:", dealer_card)
+        print("Score Of The Dealer:", dealer_score)
+        print("Cards Player Has:", player_card)
+        print("Score Of The Player:", player_score)
+        print("Dealer wins (Dealer Has High Score than Player)")
+    else:
+        print("Cards Dealer Has:", dealer_card)
+        print("Score Of The Dealer:", dealer_score)
+        print("Cards Player Has:", player_card)
+        print("Score Of The Player:", player_score)
+        print("It's a tie.")
         
-        while dealer_score < 17:
-            new_card = deck.pop()
-            dealer_card.append(new_card)
-            dealer_score += card_value(new_card)
-
-        print("Cards Dealer has:", dealer_card)
-        print("Score of the dealer:", dealer_score)
-        print("\n")
-
-        if dealer_score > 21:
-            print("Cards Dealer Has:", dealer_card)
-            print("Score Of The Dealer:", dealer_score)
-            print("Cards Player Has:", player_card)
-            print("Score Of The Player:", player_score)
-            print("Player wins (Dealer Loss Because Dealer Score is exceeding 21)")
-        elif player_score > dealer_score:
-            print("Cards Dealer Has:", dealer_card)
-            print("Score Of The Dealer:", dealer_score)
-            print("Cards Player Has:", player_card)
-            print("Score Of The Player:", player_score)
-            print("Player wins (Player Has High Score than Dealer)")
-        elif dealer_score > player_score:
-            print("Cards Dealer Has:", dealer_card)
-            print("Score Of The Dealer:", dealer_score)
-            print("Cards Player Has:", player_card)
-            print("Score Of The Player:", player_score)
-            print("Dealer wins (Dealer Has High Score than Player)")
-        else:
-            print("Cards Dealer Has:", dealer_card)
-            print("Score Of The Dealer:", dealer_score)
-            print("Cards Player Has:", player_card)
-            print("Score Of The Player:", player_score)
-            print("It's a tie.")
-            
-
