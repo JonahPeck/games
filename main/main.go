e77fad31b3ffb8ab0c80ff05f7f4d10401b153d9
JonahPeck <PeckJonahC@gmail.com>
2023-11-15 22:00:13 -0500

    new idea

--- main/main.go
+++ main/main.go
@@ -5,9 +5,16 @@ import (
 )
 
 func main() {
+	var firstCurrency string
+	var secondCurrency string
+	// var firstAmount float64
+	// var convertedAmount float64
 	fmt.Println("Currency Converter")
-	fmt.Println(`Please select your first currency: n/`)
+	fmt.Println(`Please select your first and second currency: `)
+	fmt.Println("Euros, Dollars, Pounds, or Yen")
+	fmt.Scan(&firstCurrency, &secondCurrency)
+	fmt.Println(`You are converting `, firstCurrency, ` to `, secondCurrency, `.`)
+
 }
 
-// go implementation with a switch and input scanning
-//use array, slice and map
+//idea to create a man united soccer app that displays team data and all the things :)
